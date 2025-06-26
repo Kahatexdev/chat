172d186a67886d62ad6161dc23893d9485e70698
notfoundra <novandram15@gmail.com>
2025-06-26 11:56:09 +0700

    nambahin dikit

--- routes/web.php
+++ routes/web.php
@@ -21,16 +21,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')->name('home');
+Route::get('/', Login::class)->name('home');
 
 Route::middleware('guest')->group(function () {
-    Route::get('login', Login::class)
-        ->name('login');
-
-    Route::get('register', Register::class)
-        ->name('register');
+    Route::get('login', Login::class)->name('login');
+    Route::get('register', Register::class)->name('register');
 });
-
 Route::get('password/reset', Email::class)
     ->name('password.request');
 
